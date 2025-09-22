43274e5748fca40726fb1e3b07f6ffca10d9025c
Wilson0420 <33279175+WilsonShen0420@users.noreply.github.com>
2025-09-22 22:49:44 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,7 +2,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyCVNYd41ENae-BblONYGFwFj8kdRyv-Mr4",
   authDomain: "trach-test.firebaseapp.com",
   projectId: "trach-test",
-  storageBucket: "trach-test.firebasestorage.app",
+  storageBucket: "trach-test.appspot.com",
   messagingSenderId: "318361732686",
   appId: "1:318361732686:web:d63db6fc7f6c3c5868651f",
   measurementId: "G-CJTHFH1KY1"
